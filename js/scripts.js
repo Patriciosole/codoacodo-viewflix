268cd4cb5ac0ec580edae0d697b28f0236b49f1b
Patricio Solé <patriciosole@gmail.com>
2022-09-28 09:49:16 -0300

    trying ajax on form

--- js/scripts.js
+++ js/scripts.js
@@ -278,11 +278,43 @@ function validateForm() {
        return false;
     }
     //return( true );
-    document.getElementsByClassName("success-message")[0].classList.add('success-message--active');
-    document.contactForm.nombre.value = "";
-    document.contactForm.email.value = "";
-    document.contactForm.mensaje.value = "";
-    return true;
+    // document.getElementsByClassName("success-message")[0].classList.add('success-message--active');
+    // document.contactForm.nombre.value = "";
+    // document.contactForm.email.value = "";
+    // document.contactForm.mensaje.value = "";
+    // return true;
+
+    // SEND MESSAGE
+    var form = document.getElementById("contact-form");
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementsByClassName("success-message")[0];
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.classList.add('success-message--active');
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
  }
 
  // HEART BUTTON
